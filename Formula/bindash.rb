bf7b454c699f83e6aaf88d8c200f979c3489ac38
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-02-10 01:53:18 -0500

    Update bindash.rb
    update libomp

--- Formula/bindash.rb
+++ Formula/bindash.rb
@@ -10,7 +10,7 @@ class Bindash < Formula
   depends_on "gcc" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "libgomp" => :build
+  depends_on "libomp" => :build
 
   uses_from_macos "zlib"
 
